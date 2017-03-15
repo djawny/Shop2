5ba4145072962ee17ec97905a0e50bd9b51fd42d
djawny <djawny@op.pl>
2017-03-15 19:16:37 +0100

    Initial commit

---
+++ app/src/main/java/com/offcasoftware/shop2/adapter/ProductAdapter.java
@@ -0,0 +1,78 @@
+package com.offcasoftware.shop2.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.offcasoftware.shop2.model.Product;
+import com.offcasoftware.shop2.view.widget.ProductCardView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by krzysztofjanik on 14.03.2017.
+ */
+
+public class ProductAdapter extends RecyclerView.Adapter implements ProductCardView.ProductCardViewInterface {
+
+    private List<Product> mItems = new ArrayList<>();
+
+    private ProductCardView.ProductCardViewInterface mListener;
+
+    public ProductAdapter(List<Product> products, ProductCardView.ProductCardViewInterface listener) {
+        mItems.addAll(products);
+        mListener = listener;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        final Context context = parent.getContext();
+        //LayoutInflater inflater = LayoutInflater.from(context);
+        final ProductCardView view = new ProductCardView(context);
+        return new ProductHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        final Product product = getItem(position);
+        ((ProductHolder) holder).bind(product);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    public Product getItem(int position) {
+        return mItems.get(position);
+    }
+
+    @Override
+    public void onProductClicked(Product product) {
+        if (mListener != null) {
+            mListener.onProductClicked(product);
+        }
+    }
+
+
+    public class ProductHolder extends RecyclerView.ViewHolder {
+
+        public ProductHolder(View itemView) {
+            super(itemView);
+        }
+
+        public void bind(Product product) {
+            ((ProductCardView) itemView).bindTo(product, ProductAdapter.this);
+        }
+    }
+
+    public void swapData(List<Product> products) {
+        if (products != null) {
+            mItems.clear();
+            mItems.addAll(products);
+            notifyDataSetChanged();
+        }
+    }
+}
---
+++ app/src/main/java/com/offcasoftware/shop2/view/widget/ProductListFragment.java
@@ -0,0 +1,90 @@
+package com.offcasoftware.shop2.view.widget;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindDimen;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import com.offcasoftware.shop2.R;
+import com.offcasoftware.shop2.adapter.ProductAdapter;
+import com.offcasoftware.shop2.model.Product;
+import com.offcasoftware.shop2.repository.ProductRepository;
+import com.offcasoftware.shop2.repository.ProductRepositoryInterface;
+
+import java.util.List;
+
+/**
+ * Created by krzysztofjanik on 13.03.2017.
+ */
+
+public class ProductListFragment extends Fragment implements ProductCardView.ProductCardViewInterface {
+
+    @BindView(R.id.product_recycler)
+    RecyclerView mRecyclerView;
+
+    public interface OnProductSelected {
+        void onProductSelected(Product product);
+
+    }
+
+    private ProductAdapter mProductAdapter;
+    private List<Product> products;
+    private OnProductSelected mListener;
+
+    private ProductRepositoryInterface mProductRepository
+            = ProductRepository.getInstance();
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_product_list, container, false);
+
+        ButterKnife.bind(this, view);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        displayData();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        if (activity instanceof OnProductSelected) {
+            mListener = (OnProductSelected) activity;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        products = mProductRepository.getProducts();
+        mProductAdapter.swapData(products);
+    }
+
+    private void displayData() {
+        products = mProductRepository.getProducts();
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mProductAdapter = new ProductAdapter(products, this);
+        mRecyclerView.setAdapter(mProductAdapter);
+    }
+
+    @Override
+    public void onProductClicked(Product product) {
+        if (mListener != null) {
+            mListener.onProductSelected(product);
+        }
+    }
+}
