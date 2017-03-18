17cbd19ab9bc7459a17ca360293f4b0418273567
djawny <djawny@op.pl>
2017-03-18 10:40:50 +0100

    loader

---
+++ app/src/main/java/com/offcasoftware/shop2/view/ContactsActivity.java
@@ -0,0 +1,15 @@
+package com.offcasoftware.shop2.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.offcasoftware.shop2.R;
+
+public class ContactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+    }
+}
