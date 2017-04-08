27e53b752674dd25a4da1a61bbf8b2d9fc7a0a27
djawny <djawny@op.pl>
2017-04-08 15:47:47 +0200

    espresso

---
+++ app/src/androidTest/java/com/offcasoftware/shop2/view/AddProductActivityTestEspresso.java
@@ -0,0 +1,30 @@
+package com.offcasoftware.shop2.view;
+
+import android.support.test.rule.ActivityTestRule;
+
+import com.offcasoftware.shop2.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+
+public class AddProductActivityTestEspresso {
+
+    @Rule
+    public ActivityTestRule<AddProductActivity> mActivityTestRule = new ActivityTestRule<>(AddProductActivity.class);
+
+    @Test
+    public void testAddProductIsDisplayed() {
+        onView(withId(R.id.product_name)).check(matches(isDisplayed()));
+        onView(withId(R.id.product_price)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.product_name)).perform(typeText("Product name"));
+        onView(withId(R.id.product_price)).perform(typeText("1"));
+
+        onView(withId(R.id.button_add_product)).perform(click());
+    }
+}
